773dbe9a99ec6694f9e65eada16aa0f47358b889
SamTech37 <87892685+SamTech37@users.noreply.github.com>
2022-07-02 23:47:15 +0800

    add quotes component

---
+++ components/quote.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+import { Container } from "@mantine/core";
+
+const Quote: FC = () => {
+  return <Container>{`features a quote here`}</Container>;
+};
+export default Quote;
